b4a27e7b320fe4794e7ac8e0c21675bc404ea47e
Jorge Luís Gregório <jlgregorio81@gmail.com>
2022-04-26 22:19:54 -0300

    Basic of Relationships OK

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    // activating the eager loading as default behavior
+    protected $with = ['telephone', 'city'];
+
+    public function telephone(){
+        return $this->hasOne(Telephone::class, 'id', 'id');
+    }
+
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
+
 }
